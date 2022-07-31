856773e0f9ae2b1d7f6e8e50145f79e91760a2ee
Leino Vladimir <leinovladimir@gmail.com>
2022-07-31 23:16:29 +0500

    refactoring as eslint

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,12 @@
-import readlineSync from "readline-sync";
-export const greetings = () => {
-  console.log("Welcome to the Brain Games!");
-  const name = readlineSync.question("May I have your name? ");
+/* eslint no-console: [0, { allow: ["warn", "error"] }] */
+
+import readlineSync from 'readline-sync';
+
+const greetings = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return null;
 };
+
+export default greetings;
