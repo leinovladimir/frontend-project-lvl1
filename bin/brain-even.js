b846acf8a4ee2f41d077d96b14cd2d8c422eeb73
Leino Vladimir <leinovladimir@gmail.com>
2022-08-02 10:28:36 +0500

    add game isEven algoritm

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -5,3 +5,23 @@ import greetings from '../src/cli.js';
 
 greetings();
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+const numList = [15, 6, 7];
+
+const checkForEven = (number) => (number % 2 === 0) ? true : false; 
+
+let expectedAnswer, win;
+for (const num of numList) {
+    (checkForEven(num)) ? expectedAnswer = 'yes' : expectedAnswer = 'no';
+    console.log(`Question: ${num}`);
+    const givenAnswer = readlineSync.question(`Your answer: `);
+    if (expectedAnswer == givenAnswer) { 
+        console.log('Correct!');
+        win = true
+    } else {
+        console.log(`'${givenAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.\nLet's try again, ${name}!`)
+        break;
+    }
+}
+
+if (win) { console.log(`Congratulations, ${name}`)}
